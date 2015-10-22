c959f536b80b5b72e6e80f87b5b9b7ea0c7d9bcd
Jorge Alberto Gómez Mendoza <jagomezmendoza@gmail.com>
2015-10-22 18:27:59 -0500

    HolaMundo

--- src/com/jorgegomez/holamundo/HolaMundo.java
+++ src/com/jorgegomez/holamundo/HolaMundo.java
@@ -4,6 +4,13 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		int f[]= new int[10];
+				f[0]=1;
+				
+				f[0]+=2;
+				System.out.println(f[0]);
+				
+				
 
 	}
 
